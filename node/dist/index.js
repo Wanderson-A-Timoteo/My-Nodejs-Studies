e0706173120e25695616af6b2fe5a2d5c8e0d269
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-02-24 19:03:21 -0400

    Adicionado exemplo de funções matematicas e inportação em CommonJS

--- node/dist/index.js
+++ node/dist/index.js
@@ -1,4 +1,10 @@
 "use strict";
+const Matematica = require('./matematica');
 let nome = 'Wanderson';
 let idade = 30;
 console.log(`Meu nome é ${nome} e eu tenho ${idade} anos.`);
+let n1 = 10;
+let n2 = 2;
+console.log(`SOMA: ${Matematica.somar(n1, n2)}`);
+console.log(`SUBTRAÇÃO: ${Matematica.subtrair(n1, n2)}`);
+console.log(`MULTIPLICAÇÃO: ${Matematica.multiplicar(n1, n2)}`);
--- node/src/index.ts
+++ node/src/index.ts
@@ -1,3 +1,12 @@
+const Matematica = require('./matematica')
+
 let nome: string = 'Wanderson';
 let idade: number = 30;
 console.log(`Meu nome é ${nome} e eu tenho ${idade} anos.`)
+
+let n1: number = 10;
+let n2: number = 2;
+
+console.log(`SOMA: ${Matematica.somar(n1, n2)}`);
+console.log(`SUBTRAÇÃO: ${Matematica.subtrair(n1, n2)}`);
+console.log(`MULTIPLICAÇÃO: ${Matematica.multiplicar(n1, n2)}`);
