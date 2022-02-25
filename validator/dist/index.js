f1502037a927cd2e9e371e6b261db3721506c544
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-02-24 20:05:17 -0400

    Adicionado exemplo de como usar a biblioteca validator

--- validator/dist/index.js
+++ validator/dist/index.js
@@ -1,2 +1,14 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const validator_1 = __importDefault(require("validator"));
+let email = 'wanderson_timoteo@hotmail.com';
+let ip = '192.190.0.12';
+// Verifica se é um e-mail valido e retorna true ou false
+console.log(validator_1.default.isEmail('wanderson_timoteo@hotmail.com'));
+console.log(validator_1.default.isEmail('gyibufiuybfuy'));
+console.log(validator_1.default.isEmail(email));
+// Verifica se é um endereço de IP valido
+console.log(validator_1.default.isIP(ip));
--- validator/src/index.ts
+++ validator/src/index.ts
@@ -1,1 +1,17 @@
 import validator from 'validator';
+
+let email = 'wanderson_timoteo@hotmail.com';
+let ip = '192.190.0.12'
+
+
+// Verifica se é um e-mail valido e retorna true ou false
+console.log( validator.isEmail('wanderson_timoteo@hotmail.com'));
+console.log( validator.isEmail('gyibufiuybfuy'));
+
+console.log( validator.isEmail(email));
+
+// Verifica se é um endereço de IP valido
+console.log( validator.isIP(ip));
+
+
+
