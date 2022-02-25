d4bb98e463b7096aa56e78cc8f1ad5442d9f912b
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-02-24 21:22:29 -0400

    Adicionado exemplo de rotas dinâmicas

--- servidor/src/server.ts
+++ servidor/src/server.ts
@@ -2,9 +2,25 @@ import express, { Request, Response} from 'express';
 
 const server = express();
 
+// Rota estática
 server.get('/', (req: Request, res: Response) => {
     res.send('Olá Mundo!')
 });
+
+// Rota dinânica
+server.get('/noticia/:slug', (req: Request, res: Response) => {
+    let slug: string = req.params.slug;
+
+    res.send(`Notícia: ${slug}`);
+});
+
+// Rota dinânica com dois valores diferentes, exemplo de busca de voo
+server.get('/voo/:origem-:destino', (req: Request, res: Response) => {
+    let { origem, destino } = req.params;
+
+    res.send(`Procurando voos de ${origem.toUpperCase()} para ${destino.toUpperCase()}`);
+});
+
 
 server.listen(5000);
 
