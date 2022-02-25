7a439f8d76f2f436db291225e3ade7ead707b2d4
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-02-24 21:50:41 -0400

    Adicionado exemplo de rotas em arquivo separado do servidor

--- servidor/src/server.ts
+++ servidor/src/server.ts
@@ -1,27 +1,10 @@
-import express, { Request, Response} from 'express';
+import express from 'express';
+import mainRoutes from './routes/index';
 
 const server = express();
 
-// Rota estática
-server.get('/', (req: Request, res: Response) => {
-    res.send('Olá Mundo!')
-});
+server.use(mainRoutes);
 
-// Rota dinânica
-server.get('/noticia/:slug', (req: Request, res: Response) => {
-    let slug: string = req.params.slug;
+server.listen(8000);
 
-    res.send(`Notícia: ${slug}`);
-});
-
-// Rota dinânica com dois valores diferentes, exemplo de busca de voo
-server.get('/voo/:origem-:destino', (req: Request, res: Response) => {
-    let { origem, destino } = req.params;
-
-    res.send(`Procurando voos de ${origem.toUpperCase()} para ${destino.toUpperCase()}`);
-});
-
-
-server.listen(5000);
-
-console.log("Servidor executando na porta 5000.")
+console.log("Servidor executando na porta 8000.")
