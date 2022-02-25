fe6b3eaa5a637b5707f5c2dd598de184fd94fb48
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-02-24 21:58:28 -0400

    Adicionado exemplo de rotas para acessar o painel administrativo de um site

--- servidor/src/server.ts
+++ servidor/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import mainRoutes from './routes/index';
+import painelRoutes from './routes/painel';
+
 
 const server = express();
 
 server.use(mainRoutes);
+
+server.use('/painel', painelRoutes);
 
 server.listen(8000);
 
